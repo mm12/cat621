c51b492dde88389a8242c1a76bfea97e1f37a46f
Catting <5874051+mm12@users.noreply.github.com>
2025-07-09 20:55:10 -0500

    loosey security for dev stuff
    Signed-off-by: Catting <5874051+mm12@users.noreply.github.com>

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,26 +8,22 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    policy.default_src :self
-    policy.script_src  :self, "ads.dragonfru.it", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/", "https://assets.freespeechcoalition.com"
-    policy.style_src   :self, :unsafe_inline
-    policy.connect_src :self, "ads.dragonfru.it", "plausible.dragonfru.it", "static1.e621.net", "static1.e926.net", "api.freespeechcoalition.com"
-    policy.object_src  :self, "static1.e621.net", "static1.e926.net"
-    policy.media_src   :self, "static1.e621.net", "static1.e926.net"
-    policy.frame_ancestors :none
-    policy.frame_src   'https://www.google.com/recaptcha/', 'https://www.recaptcha.net/'
-    policy.font_src    :self
-    policy.img_src     :self, :data, 'static1.e621.net', 'static1.e926.net', 'ads.dragonfru.it', 'cat621ng', 'cat621.localhost', 'e621.localhost', 'http://localhost:3000', "https://reverser.yiff.today"
-    policy.child_src   :none
-    policy.form_action :self, 'discord.e621.net', 'discord.com'
-    # Specify URI for violation reports
+    policy.default_src :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.script_src  :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.style_src   :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.connect_src :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.object_src  :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.media_src   :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.frame_ancestors :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.frame_src   :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.font_src    :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.img_src     :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.child_src   :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
+    policy.form_action :self, :unsafe_inline, :unsafe_eval, '*', :data, :blob
     # policy.report_uri "/csp-violation-report-endpoint"
   end
 
-  # Generate session nonces for permitted importmap and inline scripts
   config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
   config.content_security_policy_nonce_directives = %w(script-src)
-
-  # Report violations without enforcing the policy.
   config.content_security_policy_report_only = false
 end
